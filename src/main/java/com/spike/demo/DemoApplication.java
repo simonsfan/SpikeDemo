80bc61277c632ca808b1b4e77ea3f4d9acb9db40
simonsfan <17802115971@163.com>
2018-12-24 18:25:11 +0800

    开启事务注解器

--- src/main/java/com/spike/demo/DemoApplication.java
+++ src/main/java/com/spike/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.spike.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class DemoApplication {
 
 	public static void main(String[] args) {
